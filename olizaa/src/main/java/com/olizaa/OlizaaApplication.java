96973c0fcd028d1446158ae6ac1ea5afc5b6ff7f
guy djoumessi <gmdnegocio@gmail.com>
2025-02-22 20:40:08 -0300

    Initial commit

---
+++ olizaa/src/main/java/com/olizaa/OlizaaApplication.java
@@ -0,0 +1,13 @@
+package com.olizaa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlizaaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OlizaaApplication.class, args);
+	}
+
+}
